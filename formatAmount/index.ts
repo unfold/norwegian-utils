6c995f98114309ae50df0f2fb1e6c8902efd45b1
Ekaterina Orlova <katrineorlova@gmail.com>
2021-08-17 13:51:24 +0200

    Use swedish locale instead of norwegian

--- formatAmount/index.ts
+++ formatAmount/index.ts
@@ -4,7 +4,12 @@ export default (amount: number, options: { decimals?: boolean; alwaysNegative?: 
     amount = -Math.abs(amount)
   }
 
-  return amount.toLocaleString('nb-NO', {
+  /*17.08.21: Bug in chrome making norwegian locale not available. 
+  Adding polyfills broke login, so let's temporarily (tm) use swedish locale here, which is not broken.
+  https://gitmemory.com/issue/formatjs/formatjs/3066/888110768
+  */
+
+  return amount.toLocaleString('sv-SE', {
     minimumFractionDigits: fractionDigits,
     maximumFractionDigits: fractionDigits,
   })
